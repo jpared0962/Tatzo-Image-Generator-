02d3de203cc1a9463216b466ccf5b81399236033
jpared0962 <yeswayjosetattoos@gmail.com>
2025-03-05 13:48:34 -0800

    .

---
+++ serverless-function-project/serverless-function-project/jest.config.js
@@ -0,0 +1,1 @@
+module.exports = {  roots: ['<rootDir>/src/functions'],  testEnvironment: 'node',  transform: {    '^.+\\.tsx?$': 'ts-jest',  },};
---
+++ serverless-function-project/serverless-function-project/pages/api/hello.ts
@@ -0,0 +1,1 @@
+import { NextApiRequest, NextApiResponse } from 'next';export default function handler(req: NextApiRequest, res: NextApiResponse) {  res.status(200).json({ ok: true });}
