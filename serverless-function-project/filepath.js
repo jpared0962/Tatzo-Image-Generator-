c7b734283f34e2609301c483dadde301f3bfb799
jpared0962 <yeswayjosetattoos@gmail.com>
2025-03-05 17:53:18 -0800

    adding the front end

---
+++ serverless-function-project/filepath.js
@@ -0,0 +1,7 @@
+// filepath: /Users/joseparedes/Documents/GitHub/Tatzo-Image-Generator-/serverless-function-project/pages/api/your-api-route.ts
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  // Function disabled
+  // return NextResponse.json({ ok: true, message: "Updated response" });
+}
